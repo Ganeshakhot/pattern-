a2c1922503445ea9fbdd82769f877ff9d7417537
Ganeshakhot <108885129+Ganeshakhot@users.noreply.github.com>
2022-07-07 20:36:06 +0530

    Add files via upload

---
+++ N.java
@@ -0,0 +1,29 @@
+public class N {
+	public static void main(String[] args) {
+		int n = 5;
+
+{ for (int j = 1; j <= n; j++)
+
+for (int i = 1; i <= n; i++) { {
+
+if (i == 1 || i == n || j == n / 2+1)
+
+System.out.print("* ");
+
+}
+
+else
+
+{
+
+System.out.print(" "); //2ws
+
+}
+
+}
+
+System.out.println();
+
+}
+	}
+}
